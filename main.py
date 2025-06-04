f41b8ce256c1062e704a39c616c1e529ac020685
Manik <maniksingh10@gmail.com>
2025-06-05 00:18:28 +0530

    Deploy check

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
+from networksecurity.components.data_transformation import DataTransformation
+from networksecurity.components.model_trainer import ModelTrainer
+
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig,TrainingPipelineConfig
+from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig,TrainingPipelineConfig, DataTransformationConfig, ModelTrainerConfig
 
 import sys
 
@@ -17,6 +20,17 @@ if __name__ == '__main__':
         data_validation_config = DataValidationConfig(train_pipeline_config)
         data_validate = DataValidation(data_ingest_artifact,data_validation_config)
         data_validate_artifact=data_validate.initiate_data_validation()
+
+        data_transform_config = DataTransformationConfig(train_pipeline_config)
+        data_transform =DataTransformation(data_validate_artifact, data_transform_config)
+        data_transform_artifact = data_transform.initiate_data_transformation()
+
+        model_trainer_config=ModelTrainerConfig(train_pipeline_config)
+        model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transform_artifact)
+        model_trainer_artifact=model_trainer.initiate_model_trainer()
+
+        logging.info("Model Training artifact created")
         
     except Exception as e:
         raise NetworkSecurityException(e,sys)
+    
